b21e255cdd533ac9705cc4b4b97f92679489855b
hannan yusop <nan_s96@yahoo.com>
2021-06-22 02:02:34 +0800

    ..

---
+++ src/hashmap/custom.java
@@ -0,0 +1,24 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class custom {
+	public static void main (String[] args) {
+		
+		HashMap<String, Integer> Melaka = new HashMap<>();
+		
+		  Melaka.put("Monday", 32);
+		  Melaka.put("Teusday", 31);
+		  Melaka.put("Wednesday", 33);
+		  Melaka.put("Thursday", 35);
+		  Melaka.put("Friday", 36);
+		  Melaka.put("Staturday", 36);
+		  Melaka.put("Sunday", 33);
+		  
+		  System.out.println(Melaka);
+	}
+	
+	
+	
+
+}
